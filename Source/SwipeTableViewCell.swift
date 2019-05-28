c0ba04d01d5a3e004329ae404e7f83a63f1202f7
emuye <emuye@superhuman.com>
2019-05-28 11:28:18 -0700

    Make cell state public so it's accessible

--- Source/SwipeTableViewCell.swift
+++ Source/SwipeTableViewCell.swift
@@ -18,7 +18,7 @@ open class SwipeTableViewCell: UITableViewCell {
     /// The object that acts as the delegate of the `SwipeTableViewCell`.
     public weak var delegate: SwipeTableViewCellDelegate?
 
-    var state = SwipeState.center
+    public var state = SwipeState.center
     var actionsView: SwipeActionsView?
 
     var scrollView: UIScrollView? {
